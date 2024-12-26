5a34a941fc81ecdb1e0b3753c6e2468bcc9ff9cf
Chandan Pandey <133705046+chandanpandey91@users.noreply.github.com>
2024-12-26 20:49:24 +0530

    Create subset sum equal to k.cpp

---
+++ subset sum equal to k.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+  bool f(int ind,int k,vector<int>&v,vector<vector<int>>&dp){
+      if(k == 0) return true;
+      if(ind == 0) return v[0]==k;
+      if(dp[ind][k]!= -1){
+          return dp[ind][k];
+      }
+      bool notTaken = f(ind-1,k,v,dp);
+      bool taken = false;
+      if(v[ind]<=k){
+          taken = f(ind-1,k-v[ind],v,dp);
+      }
+      return dp[ind][k] = taken || notTaken;
+  }
+  public:
+    bool isSubsetSum(vector<int>&v, int k) {
+        int n = v.size();
+        vector<vector<int>>dp(n,vector<int>(k+1,-1));
+        return f(n-1,k,v,dp);
+    }
+};
